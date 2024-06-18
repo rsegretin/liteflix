a53993867292210fc2ded9cba07ccd22e6df50bf
rsegretin <rsegretin@yahoo.com.ar>
2024-06-18 14:38:41 -0300

    vercer server url

--- web/src/helpers/const.js
+++ web/src/helpers/const.js
@@ -1,3 +1,3 @@
 export const SELECTION_POPULAR = 'popular';
 export const SELECTION_MY_MOVIES = 'my-movies';
-export const SERVER_BASE_URL = 'https://liteflix-hilhhkyvc-richards-projects-640bae7a.vercel.app';
+export const SERVER_BASE_URL = 'https://liteflix-pied.vercel.app/';
